66821a40c18e7de5f722a1d742f986525b3ec4f9
thiloho <123883702+thiloho@users.noreply.github.com>
2025-10-24 11:21:28 +0200

    Initialize nixos server config

---
+++ server/default.nix
@@ -0,0 +1,73 @@
+{ pkgs }:
+{
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  time.timeZone = "Europe/Amsterdam";
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking = {
+    networkmanager.enable = true;
+    hostName = "nixos-server";
+    firewall = {
+      allowedTCPPorts = [
+        80
+        443
+      ];
+      allowedUDPPorts = [
+        80
+        443
+      ];
+    };
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+      settings.PasswordAuthentication = false;
+    };
+    nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+      recommendedOptimisation = true;
+      virtualHosts = {
+        "redlib.thilohohlt.com" = {
+          enableACME = true;
+          forceSSL = true;
+        };
+      };
+    };
+  };
+
+  security = {
+    acme = {
+      acceptTerms = true;
+      defaults.email = "contact@thilohohlt.com";
+    };
+  };
+
+  users.users.thohlt = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+    openssh.authorizedKeys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKDvf71FYha3PYUlOfc1rh+qZaGd6zzqYAIfecV2K6td thohlt@archlinux"
+    ];
+  };
+
+  system.stateVersion = "25.10";
+}
